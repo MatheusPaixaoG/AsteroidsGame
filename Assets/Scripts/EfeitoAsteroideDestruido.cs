72aa9b783311df4f45f4c6e3a8a51c216f726f3b
Matheus Paixão <mpgs@cin.ufpe.br>
2022-02-22 17:49:54 -0300

    Adição de partículas na destruição de um asteroide

--- Assets/Scripts/EfeitoAsteroideDestruido.cs
+++ Assets/Scripts/EfeitoAsteroideDestruido.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EfeitoAsteroideDestruido : MonoBehaviour
 {
+  public ParticleSystem meuParticleSystem;
   public AudioSource meuAudioSource;
   public float delayAutoDestruir = 1.0f;
 
   // Start is called before the first frame update
   void Start()
   {
+    meuParticleSystem.Play(false);
     meuAudioSource.Play();
     Destroy(gameObject, delayAutoDestruir);
   }
